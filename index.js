a3d88c3ddf7f92e4b5d4bfc741e21b63443c3277
muntasir23 <113699447+muntasir23@users.noreply.github.com>
2024-08-12 00:18:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const app = express();
+
+// Serve the db.json file on /books route
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
+// Middlewares
+app.use(express.json());
 app.use('/books', router);
 
+// Set the port for the server
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
